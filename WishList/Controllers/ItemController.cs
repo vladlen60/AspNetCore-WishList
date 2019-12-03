b9326eb51caa0531bc9709b4b08391ef5372793b
VladK <Vladk@iqmetrix.com>
2019-12-02 16:40:49 -0800

    Completed module

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -29,6 +29,7 @@ namespace WishList.Controllers
             return View("Create");
         }
 
+        [HttpPost]
         public IActionResult Create(Models.Item item)
         {
             _context.Items.Add(item);
